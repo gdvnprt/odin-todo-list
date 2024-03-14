288fd716080a1edffed1c2a75e3dcb6499fd2247
Gavin Davenport <gavinkdavenport@gmail.com>
2024-03-14 16:12:50 -0700

    Establish base structure
    webpack initialized with folder structure and base scripts

---
+++ src/todo.js
@@ -0,0 +1,8 @@
+//create to do item
+
+const newToDo = function(ttle, desc, due, prio) {
+    const title = ttle;
+    const description = desc;
+    let dueDate = due;
+    let priority = prio;
+};

0ccaf3bc2a8bb037b7e481e1a4a42a7149935251
Gavin Davenport <gavinkdavenport@gmail.com>
2024-03-15 17:19:54 -0700

    Build new project form
    can now submit a new project into the project list

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import newToDo from './todo.js';
 import newProject from './project.js';
 import accessStorage from './storage.js';
 
-console.log("Webpack Running");
-
-// on load, pull projects from local storage
-// new project button populates DOM and local storage list
-// new to-do button adds a to-do item within the project
-// projects and to-do items have a checkbox to mark complete
-// projects and to-do items display as complete or incomplete
-
 const projectDisplay = document.querySelector('#project-list');
+const projInfo = document.querySelector('#proj-info');
 const toDoDisplay = document.querySelector('#todo');
 
-let projectList = accessStorage();
-
+//clearing functions
 function clearProjects() {
     while (projectDisplay.firstChild) {
         projectDisplay.removeChild(projectDisplay.firstChild);
@@ -26,3 +18,35 @@ function clearToDo() {
         toDoDisplay.removeChild(projectDisplay.firstChild);
     };
 };
+
+// on load, pull projects from local storage
+let projectList = accessStorage();
+
+// new project button populates DOM and local storage list
+const newProjBtn = document.querySelector('#new-project');
+const newProjDia = document.querySelector('new-proj-dialogue');
+newProjDia.open = false;
+
+newProjBtn.addEventListener('click', () => {
+    newProjDia.open = true
+});
+
+const projForm = document.querySelector('#new-proj-form');
+
+projForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fd = new FormData(projForm);
+    const formObj = Object.fromEntries(fd);
+    projectList.push(newProject(formObj.title, '', '', ''));
+    clearProjects();
+    // add populate projects
+    clearToDo();
+    // add populate toDo
+    newProjDia.open = false;
+    projForm.reset();
+});
+
+// new to-do button adds a to-do item within the project
+// projects and to-do items have a checkbox to mark complete
+// projects and to-do items display as complete or incomplete
+// can rearrange placement of projects and to-do items
--- src/project.js
+++ src/project.js
@@ -1,7 +1,8 @@
-export default function newProject(ttle, desc, due) {
+export default function newProject(ttle, desc, due, prio) {
     const title = ttle;
     const description = desc;
-    let dueDate = due;
+    let dueDate = due; 
+    let priority = prio
     let list = [];
     let done = false
 

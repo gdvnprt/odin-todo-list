940231be7699443ac94854b651ba8538f42f9d1a
Gavin Davenport <gavinkdavenport@gmail.com>
2024-03-15 17:38:02 -0700

    Conceptualize dom populating functions
    pseudocode to follow for DOM manipulation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,26 @@ const projectDisplay = document.querySelector('#project-list');
 const projInfo = document.querySelector('#proj-info');
 const toDoDisplay = document.querySelector('#todo');
 
+//populating functions
+function populateProjects() {
+    // loop for all projects
+    // create a project div within projectDisplay
+    // make the div able to be highlighted on click
+    // make the same event trigger populating ToDo with a separate function
+    // when another project is clicked, all others remove highlight
+    // show project title and due date if incomplete and complete if complete
+};
+
+function populateToDo() {
+    // populate project info section with title, desc, due date
+    // have methods to change all project info
+    // project info has a button to mark the project complete or incomplete
+    // loop the below for all toDo items
+    // create a div within toDoDisplay
+    // populate the div with title, description, priority, due date
+    // div has a prominent checkbox to change complete status
+};
+
 //clearing functions
 function clearProjects() {
     while (projectDisplay.firstChild) {
@@ -24,7 +44,7 @@ let projectList = accessStorage();
 
 // new project button populates DOM and local storage list
 const newProjBtn = document.querySelector('#new-project');
-const newProjDia = document.querySelector('new-proj-dialogue');
+const newProjDia = document.querySelector('#new-proj-dialogue');
 newProjDia.open = false;
 
 newProjBtn.addEventListener('click', () => {
@@ -37,9 +57,10 @@ projForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const fd = new FormData(projForm);
     const formObj = Object.fromEntries(fd);
-    projectList.push(newProject(formObj.title, '', '', ''));
+    projectList.unshift(newProject(formObj.title, '', '', ''));
     clearProjects();
     // add populate projects
+    // highlight the new project as if selected
     clearToDo();
     // add populate toDo
     newProjDia.open = false;
